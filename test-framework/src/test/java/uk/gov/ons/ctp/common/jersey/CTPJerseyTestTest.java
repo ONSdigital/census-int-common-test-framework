e0d17074e562af0d14f76bb198af071f055bbe47
Phil Whiles <philip.whiles@ons.gov.uk>
2016-03-04 12:27:19 +0000

    CTP-247 TestTest to use a diff port to real tests and avoid CI collision

--- test-framework/src/test/java/uk/gov/ons/ctp/common/jersey/CTPJerseyTestTest.java
+++ test-framework/src/test/java/uk/gov/ons/ctp/common/jersey/CTPJerseyTestTest.java
@@ -2,6 +2,7 @@ package uk.gov.ons.ctp.common.jersey;
 
 import javax.ws.rs.core.Application;
 
+import org.glassfish.jersey.test.TestProperties;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 
@@ -12,18 +13,21 @@ public class CTPJerseyTestTest extends CTPJerseyTest {
 
   @Override
   public Application configure() {
+    // set the port to differ from that used by the real tests (9998) to avoid collision
+    // when CI server runs parallel builds
+    forceSet(TestProperties.CONTAINER_PORT, "9997");
     return super.init(HelloWorldEndpoint.class, null, null, null);
   }
 
   @Test
   public void testResponseCodesPass() {
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertResponseCodeIs(HttpStatus.OK)
      .andClose();
   }
   @Test(expected=AssertionError.class)
   public void testResponseCodesFail() {
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertResponseCodeIs(HttpStatus.I_AM_A_TEAPOT)
      .andClose();
   }
@@ -31,37 +35,37 @@ public class CTPJerseyTestTest extends CTPJerseyTest {
 
   @Test
   public void testResponseBodyPass() {
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertStringInBody("$.hairColour", "brown")
      .andClose();
   }
   @Test(expected=AssertionError.class)
   public void testResponseBodyFail() {
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertStringInBody("$.hairColour", "bright pink")
      .andClose();
   }
 
   @Test
   public void testResponseListPass() {
-    with("http://localhost:9998/hello/list")
+    with("http://localhost:9997/hello/list")
      .assertArrayLengthInBodyIs(2)
      .andClose();
   }
   @Test(expected=AssertionError.class)
   public void testResponseListFail() {
-    with("http://localhost:9998/hello/list")
+    with("http://localhost:9997/hello/list")
      .assertArrayLengthInBodyIs(100)
      .andClose();
   }
 
   public void testAssertionOrderCanBeChanged() {
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertResponseCodeIs(HttpStatus.OK)
      .assertStringInBody("$.hairColour", "brown")
      .andClose();
 
-    with("http://localhost:9998/hello/%s", "world")
+    with("http://localhost:9997/hello/%s", "world")
      .assertStringInBody("$.hairColour", "brown")
      .assertResponseCodeIs(HttpStatus.OK)
      .andClose();
@@ -69,7 +73,7 @@ public class CTPJerseyTestTest extends CTPJerseyTest {
   
   @Test
   public void testExceptionTesting() {
-    with("http://localhost:9998/hello/ctpexception/%s", "world")
+    with("http://localhost:9997/hello/ctpexception/%s", "world")
     .assertResponseCodeIs(HttpStatus.NOT_FOUND)
     .assertFaultIs(CTPException.Fault.RESOURCE_NOT_FOUND)
     .assertTimestampExists()
