3fab10b0c5612aa1f2378c880f1f99b536c189a6
Philippe Brossier <philippe.brossier@ons.gov.uk>
2016-03-17 09:25:25 +0000

    CTPA-315 Generic message body reader now dealing with javax validations

--- framework/src/main/java/uk/gov/ons/ctp/common/utility/CTPMessageBodyReader.java
+++ framework/src/main/java/uk/gov/ons/ctp/common/utility/CTPMessageBodyReader.java
@@ -5,6 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
@@ -15,6 +19,7 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.Set;
 
 /**
  * This class is the generic CTP MessageBodyReader. It will be instantiated with the relevant type in JerseyConfig for
@@ -31,6 +36,12 @@ public class CTPMessageBodyReader<T> implements MessageBodyReader<T> {
     this.theType = aType;
   }
 
+  private static Validator validator;
+  static {
+    ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+    validator = factory.getValidator();
+  }
+  
   @Override
   public final boolean isReadable(final Class<?> type, final Type genericType, final Annotation[] annotations,
       final MediaType mediaType) {
@@ -41,7 +52,7 @@ public class CTPMessageBodyReader<T> implements MessageBodyReader<T> {
   public final T readFrom(final Class<T> type, final Type genericType,
       final Annotation[] annotations, final MediaType mediaType, final MultivaluedMap<String, String> httpHeaders,
       final InputStream entityStream) throws IOException, WebApplicationException {
-    log.debug("Entering readFrom...");
+    log.debug("Entering readFrom with theType= {} ", theType);
     try {
       StringWriter writer = new StringWriter();
       IOUtils.copy(entityStream, writer, "UTF-8");
@@ -51,7 +62,15 @@ public class CTPMessageBodyReader<T> implements MessageBodyReader<T> {
       ObjectMapper mapper = new ObjectMapper();
       T requestObject = mapper.readValue(requestJson, theType);
       log.debug("requestObject = {}", requestObject);
-      return requestObject;
+
+      Set<ConstraintViolation<T>> constraintViolations = validator.validate(requestObject);
+      int numberOfViolations = constraintViolations.size();
+      if (numberOfViolations > 0) {
+        log.error("{} constraints have been violated.", numberOfViolations);
+        return null;
+      } else {
+        return requestObject;
+      }
     } catch (Exception e) {
       log.error("Exception thrown while reading request body - {}", e);
       return null;
