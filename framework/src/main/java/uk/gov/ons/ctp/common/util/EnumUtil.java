c5b6c15ecd3e975b2c87925eae3f970695ac3d08
Phil Whiles <philip.whiles@ons.gov.uk>
2016-10-21 13:59:40 +0100

    CTPA-799 util

---
+++ framework/src/main/java/uk/gov/ons/ctp/common/util/EnumUtil.java
@@ -0,0 +1,24 @@
+package uk.gov.ons.ctp.common.util;
+
+public class EnumUtil {
+  public static <T extends Enum <T>> T getEnumFromString(Class<T> enumClass, String value) {
+    if (enumClass == null) {
+      throw new IllegalArgumentException("cant be null");
+      
+    }
+    
+    for (Enum<?> enumValue : enumClass.getEnumConstants()) {
+      if (enumValue.toString().equalsIgnoreCase(value)) {
+       return (T) enumValue;
+      }
+    }
+    
+    StringBuilder errorMessage = new StringBuilder();
+    boolean firstTime = true;
+    for (Enum<?> enumValue : enumClass.getEnumConstants()) {
+      errorMessage.append(firstTime ? "" :", ").append(enumValue);
+      firstTime = false;
+    }
+    throw new IllegalArgumentException(value + " is invalid value. only " + errorMessage);
+  }
+}
