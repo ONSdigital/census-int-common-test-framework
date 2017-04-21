8752b818c18f846ee40250780d4aa3a854298f76
Philippe Brossier <brossierp@gmail.com>
2017-04-21 11:03:55 +0100

    ctpa1187 uping version

---
+++ test-framework/src/main/java/uk/gov/ons/ctp/common/MvcHelper.java
@@ -0,0 +1,19 @@
+package uk.gov.ons.ctp.common;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+public class MvcHelper {
+    public static MockHttpServletRequestBuilder getJson(String url) {
+        return get(url).accept(MediaType.APPLICATION_JSON);
+    }
+
+    public static MockHttpServletRequestBuilder postJson(String url, String content) {
+        return post(url).content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+    }
+}
