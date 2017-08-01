12f1af65c1fee5b1b2ce3d9ee0d55277b36ad497
Philippe Brossier <brossierp@gmail.com>
2017-08-01 16:20:45 +0100

    CTPA-1421 Uping version of Orika

--- framework/src/main/java/uk/gov/ons/ctp/common/util/StringToUUIDConverter.java
+++ framework/src/main/java/uk/gov/ons/ctp/common/util/StringToUUIDConverter.java
@@ -1,21 +1,17 @@
 package uk.gov.ons.ctp.common.util;
 
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.metadata.Type;
 
 import java.util.UUID;
 
-/**
- * Created by stevee on 14/06/2017.
- */
 public class StringToUUIDConverter extends BidirectionalConverter<String, UUID> {
-
-  public UUID convertTo(String source, Type<UUID> destinationType) {
+  public UUID convertTo(String source, Type<UUID> destinationType, MappingContext mappingContext) {
     return UUID.fromString(source);
   }
 
-  public String convertFrom(UUID source, Type<String> destinationType) {
+  public String convertFrom(UUID source, Type<String> destinationType, MappingContext mappingContext) {
     return source.toString();
   }
-
 }
