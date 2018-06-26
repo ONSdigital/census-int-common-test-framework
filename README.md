3e8b3e56cf3d3063acbaffd5cf95b7db5890f194
Ben Jefferies <benjefferies@echosoft.uk>
2018-06-26 15:37:31 +0100

    Add formatter and checkstyle
    Adding maven plugin to format the java code automatically on builds. The
    style of the formatter is opinionated favouring googles formatting. The
    benefit of this is it allows us to easily plugin in googles check style
    file.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.org/ONSdigital/rm-common-service.svg?branch=master)](https://travis-ci.org/ONSdigital/rm-common-service)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/37fdebe43c0f467ead6394a3d43d90f4)](https://www.codacy.com/app/sdcplatform/rm-common-service?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=ONSdigital/rm-common-service&amp;utm_campaign=Badge_Grade)
 
-# Common Test Framework 
+# Common Test Framework
 This repository contains common test framework code for the Response Management [Spring Boot](http://projects.spring.io/spring-boot/) applications.
 
 This project provides the base class for the ONS Spring Boot/Jersey unit tests, which provides a Domain-Specific Language (DSL) for unit testing RESTful endpoints.
@@ -11,5 +11,9 @@ This project provides the base class for the ONS Spring Boot/Jersey unit tests, 
 ```
 mvn --update-snapshots
 ```
+
+## Code Styler
+To use the code styler please goto this url (https://github.com/google/google-java-format) and follow the Intellij instructions or Eclipse depending on what you use
+
 ## Copyright
 Copyright (C) 2016 Crown Copyright (Office for National Statistics)
--- src/main/java/uk/gov/ons/ctp/common/FixtureHelper.java
+++ src/main/java/uk/gov/ons/ctp/common/FixtureHelper.java
@@ -1,18 +1,13 @@
 package uk.gov.ons.ctp.common;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.util.Arrays;
 import java.util.List;
-
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Loads JSON representation of test DTOS for unit tests
- *
- */
+/** Loads JSON representation of test DTOS for unit tests */
 @Slf4j
 public class FixtureHelper {
 
@@ -22,8 +17,8 @@ public class FixtureHelper {
    * @param <T> the type of object we expect to load and return a List of
    * @param clazz the type
    * @return the list
-   * @throws Exception failed to load - does the json file exist alongside the
-   *           calling class in the classpath?
+   * @throws Exception failed to load - does the json file exist alongside the calling class in the
+   *     classpath?
    */
   public static <T> List<T> loadMethodFixtures(final Class<T[]> clazz) throws Exception {
     String callerClassName = new Exception().getStackTrace()[1].getClassName();
@@ -31,20 +26,19 @@ public class FixtureHelper {
   }
 
   /**
-   * Find, deserialize and return List of dummy test objects from a json file
-   * This method derives the path and file name of the json file by looking at
-   * the class and method that called it, as well as the name of the type you
-   * asked it to return.
+   * Find, deserialize and return List of dummy test objects from a json file This method derives
+   * the path and file name of the json file by looking at the class and method that called it, as
+   * well as the name of the type you asked it to return.
    *
    * @param <T> the type of object we expect to load and return a List of
    * @param clazz the type
-   * @param qualifier added to file name to allow a class to have multiple forms
-   *          of same type
+   * @param qualifier added to file name to allow a class to have multiple forms of same type
    * @return the list
-   * @throws Exception failed to load - does the json file exist alongside the
-   *           calling class in the classpath?
+   * @throws Exception failed to load - does the json file exist alongside the calling class in the
+   *     classpath?
    */
-  public static <T> List<T> loadMethodFixtures(final Class<T[]> clazz, final String qualifier) throws Exception {
+  public static <T> List<T> loadMethodFixtures(final Class<T[]> clazz, final String qualifier)
+      throws Exception {
     String callerClassName = new Exception().getStackTrace()[1].getClassName();
     String callerMethodName = new Exception().getStackTrace()[1].getMethodName();
     return actuallyLoadFixtures(clazz, callerClassName, callerMethodName, qualifier);
@@ -56,8 +50,8 @@ public class FixtureHelper {
    * @param <T> the type of object we expect to load and return a List of
    * @param clazz the type
    * @return the list
-   * @throws Exception failed to load - does the json file exist alongside the
-   *           calling class in the classpath?
+   * @throws Exception failed to load - does the json file exist alongside the calling class in the
+   *     classpath?
    */
   public static <T> List<T> loadClassFixtures(final Class<T[]> clazz) throws Exception {
     String callerClassName = new Exception().getStackTrace()[1].getClassName();
@@ -65,23 +59,21 @@ public class FixtureHelper {
   }
 
   /**
-   * Find, deserialize and return List of dummy test objects from a json file
-   * This method derives the path and file name of the json file by looking at
-   * the class and method that called it, as well as the name of the type you
-   * asked it to return.
+   * Find, deserialize and return List of dummy test objects from a json file This method derives
+   * the path and file name of the json file by looking at the class and method that called it, as
+   * well as the name of the type you asked it to return.
    *
    * @param <T> the type of object we expect to load and return a List of
    * @param clazz the type
-   * @param qualifier added to file name to allow a class to have multiple forms
-   *          of same type
+   * @param qualifier added to file name to allow a class to have multiple forms of same type
    * @return the list
-   * @throws Exception failed to load - does the json file exist alongside the
-   *           calling class in the classpath?
+   * @throws Exception failed to load - does the json file exist alongside the calling class in the
+   *     classpath?
    */
-  public static <T> List<T> loadClassFixtures(final Class<T[]> clazz, final String qualifier) throws Exception {
+  public static <T> List<T> loadClassFixtures(final Class<T[]> clazz, final String qualifier)
+      throws Exception {
     String callerClassName = new Exception().getStackTrace()[1].getClassName();
     return actuallyLoadFixtures(clazz, callerClassName, null, qualifier);
-
   }
 
   /**
@@ -91,13 +83,16 @@ public class FixtureHelper {
    * @param <T> the type of object we expect to load and return a List of
    * @param callerClassName name of the class that made the initial call
    * @param callerMethodName name of the method that made the initial call
-   * @param qualifier added to file name to allow a class to have multiple forms
-   *          of same type
+   * @param qualifier added to file name to allow a class to have multiple forms of same type
    * @return the loaded dummies of the the type T in a List
    * @throws Exception summats went wrong
    */
-  private static <T> List<T> actuallyLoadFixtures(final Class<T[]> clazz, final String callerClassName,
-      final String callerMethodName, final String qualifier) throws Exception {
+  private static <T> List<T> actuallyLoadFixtures(
+      final Class<T[]> clazz,
+      final String callerClassName,
+      final String callerMethodName,
+      final String qualifier)
+      throws Exception {
     List<T> dummies = null;
     ObjectMapper mapper = new ObjectMapper();
     mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
@@ -119,16 +114,22 @@ public class FixtureHelper {
    *
    * @param callerClassName the name of the class that made the initial call
    * @param clazzName the type of object to deserialize and return in a List
-   * @param methodName the name of the method in the callerClass that made the
-   *          initial call
-   * @param qualifier further quaification is a single method may need to have
-   *          two collections of the same type, qualified
+   * @param methodName the name of the method in the callerClass that made the initial call
+   * @param qualifier further quaification is a single method may need to have two collections of
+   *     the same type, qualified
    * @return the constructed path string
    */
-  private static String generatePath(final String callerClassName, final String clazzName, final String methodName,
+  private static String generatePath(
+      final String callerClassName,
+      final String clazzName,
+      final String methodName,
       final String qualifier) {
-    return callerClassName.replaceAll("\\.", "/") + "." + ((methodName != null) ? (methodName + ".") : "") + clazzName
+    return callerClassName.replaceAll("\\.", "/")
         + "."
-        + ((qualifier != null) ? (qualifier + ".") : "") + "json";
+        + ((methodName != null) ? (methodName + ".") : "")
+        + clazzName
+        + "."
+        + ((qualifier != null) ? (qualifier + ".") : "")
+        + "json";
   }
 }
