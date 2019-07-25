b1531ab1a841e478648bcec5829e5819f5ad4339
Peter Bochel <Peter.Bochel@ext.ons.gov.uk>
2019-07-25 14:20:28 +0100

    CR-347 Checkstyle changes

--- src/main/java/uk/gov/ons/ctp/common/firestore/FirestoreWait.java
+++ src/main/java/uk/gov/ons/ctp/common/firestore/FirestoreWait.java
@@ -16,15 +16,31 @@ import uk.gov.ons.ctp.common.error.CTPException.Fault;
 public class FirestoreWait {
   private static final Logger log = LoggerFactory.getLogger(FirestoreWait.class);
 
+  // This is the name of the collection to search, eg, 'case'
   @NonNull private String collection;
 
+  // This is the key of the target object in the collection. eg,
+  // 'f868fcfc-7280-40ea-ab01-b173ac245da3'
   @NonNull private String key;
 
+  // This is an optional argument to specify the timestamp that the an object must have
+  // been updated after. Waiting will continue until the until the update timestamp of the
+  // object is greater than this value, or the timeout period is reached. This value is the
+  // number of milliseconds since the epoch.
   private Long newerThan;
 
+  // This is an optional path to a field whose content we check to decide if an
+  // object has been updated, eg, 'contact.forename' or 'state'. If the target object does not
+  // contain the field with the expected value then waiting will continue until it does, or the
+  // timeout is reached.
   private String contentCheckPath;
+
+  // This is the value that a field must contain if 'contentCheckPath' has been specified.
   private String expectedValue;
 
+  // This specifies how long the caller is prepared to wait for an object to appear in
+  // Firestore. This string must end with either a 'ms' suffix for milliseconds or 's' for
+  // seconds, eg, '750ms', '10s or '2.5s'.
   @NonNull private String timeout;
 
   /**
@@ -36,31 +52,15 @@ public class FirestoreWait {
    * timestamp or by the content of a named field. If both criteria are specified then both
    * conditions must be satisfied before we regard the object has having arrived in Firestore.
    *
-   * @param collection is the name of the collection to search, eg, 'case'
-   * @param key is the key of the target object in the collection. eg,
-   *     'f868fcfc-7280-40ea-ab01-b173ac245da3'
-   * @param newerThan, is an optional argument to specify the timestamp that the an object must have
-   *     been updated after. Waiting will continue until the until the update timestamp of the
-   *     object is greater than this value, or the timeout period is reached. This value is the
-   *     number of milliseconds since the epoch.
-   * @param contentCheckPath, is an optional path to a field whose content we check to decide if an
-   *     object has been updated, eg, 'contact.forename' or 'state'. If the target object does not
-   *     contain the field with the expected value then waiting will continue until it does, or the
-   *     timeout is reached.
-   * @param expectedValue, is the value that a field must contain if 'contentCheckPath' has been
-   *     specified.
-   * @param timeout specifies how long the caller is prepared to wait for an object to appear in
-   *     Firestore. This string must end with either a 'ms' suffix for milliseconds or 's' for
-   *     seconds, eg, '750ms', '10s or '2.5s'.
    * @return The update timestamp of a found object, or null if not found within the timeout.
    * @throws CTPException in the event of a failure being detected. This will be of type
    *     Fault.VALIDATION_FAILED if any arguments fail validation, or type Fault.SYSTEM_ERROR if
    *     there is a Firestore exception.
    */
   public Long waitForObject() throws CTPException {
-    long startTime = System.currentTimeMillis();
-    long timeoutMillis = parseTimeoutString(timeout);
-    long timeoutLimit = startTime + timeoutMillis;
+    final long startTime = System.currentTimeMillis();
+    final long timeoutMillis = parseTimeoutString(timeout);
+    final long timeoutLimit = startTime + timeoutMillis;
 
     log.info(
         "Firestore wait. Looking for for collection '"
